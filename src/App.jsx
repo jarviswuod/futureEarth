41ab8f8e7699505b7f431f920b2e6968b22bf350
jarvisthedev <johnsonmakings@gmail.com>
2024-06-06 10:31:34 +0300

    Setting project base structure with some preinstallations

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 
-const App = () => {
-  return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </div>
-  );
-};
+const App = () => (
+  <main className="relative">
+    <nav>Nav</nav>
+    <section className="xl:paddin-1 wide:padding-r padding-b">Hero</section>
+    <section className="padding">PopularProducts</section>
+    <section className="padding">Superquality</section>
+    <section className="padding py-10">services</section>
+    <section className="padding">SpecialOffer</section>
+    <section className="padding bg-pale-blue">CustomerReview</section>
+    <section className="padding-x sm:py-32 py-16 w-full">Subscribe</section>
+    <section className="bg-black padding-x padding-t pb-8">Footer</section>
+  </main>
+);
 
 export default App;
