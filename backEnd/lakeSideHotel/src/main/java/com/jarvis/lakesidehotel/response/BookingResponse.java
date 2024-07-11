417452909ab20c9b25967fdb19f317657fc85dde
jarvisthedev <johnsonmakings@gmail.com>
2024-07-11 23:28:01 +0300

    Started building room and roomBooking endpoint creation. Connected project to mySQL db

---
+++ backEnd/lakeSideHotel/src/main/java/com/jarvis/lakesidehotel/response/BookingResponse.java
@@ -0,0 +1,31 @@
+package com.jarvis.lakesidehotel.response;
+
+import com.jarvis.lakesidehotel.model.Room;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class BookingResponse {
+    private Long id;
+    private LocalDate checkInDate;
+    private LocalDate checkOutDate;
+    private String guestFullName;
+    private String guestEmail;
+    private int numberOfAdults;
+    private int numberOfChildren;
+    private int totalNumberOfGuests;
+    private String bookingConfirmationCode;
+
+    public BookingResponse(Long id, LocalDate checkInDate, LocalDate checkOutDate, String bookingConfirmationCode) {
+        this.id = id;
+        this.checkInDate = checkInDate;
+        this.checkOutDate = checkOutDate;
+        this.bookingConfirmationCode = bookingConfirmationCode;
+    }
+
+    private RoomResponse room;
+}
