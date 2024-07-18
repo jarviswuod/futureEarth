1a5afd04d631da96f5b875e6a9f41b0bfea423bd
jarvisthedev <johnsonmakings@gmail.com>
2024-07-18 12:10:33 +0300

    FrontEnd stlying plus pages & components addition

--- frontEnd/lakeSideHotel/src/components/home/Home.jsx
+++ frontEnd/lakeSideHotel/src/components/home/Home.jsx
@@ -2,14 +2,20 @@ import React from "react";
 import MainHeader from "../common/MainHeader";
 import Parallax from "../common/Parallax";
 import HotelService from "../common/HotelService";
+import RoomCarousel from "../common/RoomCarousel";
 
 const Home = () => {
   return (
     <section>
       <MainHeader />
       <section className="container">
+        <RoomCarousel />
         <Parallax />
+        <RoomCarousel />
+
         <HotelService />
+        <RoomCarousel />
+
         <Parallax />
       </section>
     </section>
